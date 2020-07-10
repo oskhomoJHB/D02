b1043ce842b16bedbcfba868a86367b9741b0fe3
Osego Khomo <oskhomo@c5r7s1.wethinkcode.co.za>
2020-07-10 13:35:13 +0200

    Commit

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+ 
+ void ft_putchar(char c)
+{
+	write(1,&c, 1);
+}
+ 
+ void ft_print_numbers(void)
+{
+	char numbers = '0';
+	while (numbers <= '9')
+	{
+		ft_putchar(numbers);
+		numbers++;
+	}
+}
+
+ int main  (void) 
+{ 
+	ft_print_numbers();
+}
+
