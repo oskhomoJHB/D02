5ed0415c90c62bdff6c4fc0b3644496e2840bea7
Osego Khomo <oskhomo@c5r7s1.wethinkcode.co.za>
2020-07-12 09:20:41 +0200

    Reverse alphabets

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,18 +1,25 @@
-#include <unistd.h>
- 
-void ft_putchar(char c)
- {
- write (1, &c, 1);
- }
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oskhomo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/11 15:56:14 by oskhomo           #+#    #+#             */
+/*   Updated: 2020/07/11 16:22:11 by oskhomo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
- void ft_print_reverse_alphabet(void)
+void	ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
 {
-   char letter  = 'z';
-   while (letter >= 'a')
-     { 
-   	ft_putchar(letter);
-	letter --;
+	char	letter;
+	
+	letter	= 'z';
+	while (letter >= 'a')
+     {
+		 ft_putchar(letter);
+		 letter --;
      }
- 
 }
-
