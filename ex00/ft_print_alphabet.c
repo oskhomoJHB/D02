72232ef7925ec2fe0c8cbb815ddf6498633508aa
Osego Khomo <oskhomo@c5r7s1.wethinkcode.co.za>
2020-07-12 09:18:21 +0200

    Printing Alphabets

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -1,17 +1,25 @@
-#include <unistd.h>  
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oskhomo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/11 15:43:43 by oskhomo           #+#    #+#             */
+/*   Updated: 2020/07/11 15:55:24 by oskhomo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void  ft_putchar(char c)
-{
-   write(1, &c, 1); 
-}
+void	ft_putchar(char c);
 
-void ft_print_alphabet(void)
+void	ft_print_alphabet(void)
 {
-	char letter = 'a';
+	char	letter;
+	
+	letter = 'a';
 	while (letter <='z')
 	{
-		  ft_putchar( letter);
+	   	ft_putchar( letter);
 		  letter ++;
 	}	
 }
-
